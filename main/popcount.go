6a25767c072a8391e75930e255a79e4d9ce1f5a3
Steve Zhang <stevzhang01@gmail.com>
2018-12-04 17:48:56 +0800

    Multiple algorithms for PopCount()

--- main/popcount.go
+++ main/popcount.go
@@ -1,3 +1,4 @@
+// Test package popcount.
 package main
 
 import (
@@ -5,16 +6,28 @@ import (
 	"go-learning/popcount"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		x, err := strconv.Atoi(arg)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
-			continue
-		}
-		count := popcount.PopCount(uint64(x))
-		fmt.Printf("%d popcount: %d\n", x, count)
+	x, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 	}
+
+	startA := time.Now()
+	countA := popcount.PopCount(uint64(x))
+	fmt.Printf("A\t%d\t%.12fs\n", countA, time.Since(startA).Seconds())
+
+	startB := time.Now()
+	countB := popcount.PopCountB(uint64(x))
+	fmt.Printf("B\t%d\t%.12fs\n", countB, time.Since(startB).Seconds())
+
+	startC := time.Now()
+	countC := popcount.PopCountC(uint64(x))
+	fmt.Printf("C\t%d\t%.12fs\n", countC, time.Since(startC).Seconds())
+
+	startD := time.Now()
+	countD := popcount.PopCountD(uint64(x))
+	fmt.Printf("D\t%d\t%.12fs\n", countD, time.Since(startD).Seconds())
 }
--- popcount/popcount.go
+++ popcount/popcount.go
@@ -1,5 +1,6 @@
 package popcount
 
+// pc[i] is the population count of i.
 var pc [256]byte
 
 func init() {
@@ -8,6 +9,7 @@ func init() {
 	}
 }
 
+// PopCount returns the population count (number of set bits) of x.
 func PopCount(x uint64) int {
 	return int(pc[byte(x>>(0*8))] +
 		pc[byte(x>>(1*8))] +
@@ -17,4 +19,31 @@ func PopCount(x uint64) int {
 		pc[byte(x>>(5*8))] +
 		pc[byte(x>>(6*8))] +
 		pc[byte(x>>(7*8))])
+}
+
+func PopCountB(x uint64) int {
+	var count byte
+	var i uint
+	for i = 0; i < 8; i++ {
+		count += pc[byte(x>>(i*8))]
+	}
+	return int(count)
+}
+
+func PopCountC(x uint64) int {
+	var count int
+	var i uint
+	for i = 0; i < 64; i++ {
+		count += int((x >> i) & 1)
+	}
+	return count
+}
+
+func PopCountD(x uint64) int {
+    var count int
+    for x != 0 {
+        x = x&(x-1)
+        count++
+    }
+    return count
 }
