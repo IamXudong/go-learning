4db08a6af3664d5a7a27951088526a3b517c9a54
Steve Zhang <stevzhang01@gmail.com>
2018-12-06 22:57:59 +0800

    修改surface.go中svg标签书写错误&生成新的svg文件

--- main/surface.go
+++ main/surface.go
@@ -17,7 +17,7 @@ const (
 var cos30, sin30 = math.Cos(angle), math.Sin(angle)
 
 func main() {
-		fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' style='stroke: grey; fill: white; stroke-width: 0.7' width='%d' height='%d'>", width, height)
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' style='stroke: grey; fill: white; stroke-width: 0.7' width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
@@ -27,7 +27,7 @@ func main() {
 			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("svg")
+	fmt.Println("</svg>")
 }
 
 func corner(i, j int) (float64, float64) {
