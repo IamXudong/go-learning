6bfd9895ff070aacd10c85c24f27402e64bc51d2
Steve Zhang <stevzhang01@gmail.com>
2018-12-07 12:21:05 +0800

    surface.go添加注释

--- main/surface.go
+++ main/surface.go
@@ -1,3 +1,4 @@
+// surface 根据一个三维曲面函数计算并生成svg
 package main
 
 import (
@@ -6,12 +7,12 @@ import (
 )
 
 const (
-	width, height = 600, 320
-	cells         = 100
-	xyrange       = 30.0
-	xyscale       = width / 2 / xyrange
-	zscale        = height * 0.4
-	angle         = math.Pi / 6
+	width, height = 600, 320            // 以像素表示的画布大小
+	cells         = 100                 // 网格单元的个数
+	xyrange       = 30.0                // 坐标轴的范围
+	xyscale       = width / 2 / xyrange // x或y轴上每个单位长度的像素
+	zscale        = height * 0.4        // z轴上每个单位长度的像素
+	angle         = math.Pi / 6         // x, y轴的角度(=30度)
 )
 
 var cos30, sin30 = math.Cos(angle), math.Sin(angle)
@@ -31,10 +32,14 @@ func main() {
 }
 
 func corner(i, j int) (float64, float64) {
+	// 求出网格单元(i, j)的顶点坐标(x, y)
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
+
+	// 计算出曲面的高度z
 	z := f(x, y)
 
+	// 将(x, y, z) 等角投射到二维SVG绘图平面上, 坐标是(sx, sy)
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 
@@ -42,6 +47,6 @@ func corner(i, j int) (float64, float64) {
 }
 
 func f(x, y float64) float64 {
-	r := math.Hypot(x, y)
+	r := math.Hypot(x, y) // 到(0, 0)的距离
 	return math.Sin(r) / r
 }
