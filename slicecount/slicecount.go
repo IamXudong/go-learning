c1e75ea8eedda004aee5667b105b9886ee5676ee
StevE Zhang <stevzhang01@gmail.com>
2019-01-07 16:00:00 +0800

    修改slicecount

--- slicecount/slicecount.go
+++ slicecount/slicecount.go
@@ -1,10 +1,15 @@
-// 使用map来记录一个字符串slice被add函数调用的次数
+// Package slicecount 使用map来记录一个字符串slice被add函数调用的次数
 package slicecount
 
 import "fmt"
 
 var count = make(map[string]int)
 
+// key 生成slice对应到map的string类型key
 func key(list []string) string { return fmt.Sprintf("%q", list) }
-func Add(list []string)        { count[key(list)]++ }
-func Count(list []string) int  { return count[key(list)] }
+
+// Add 添加次数
+func Add(list []string) { count[key(list)]++ }
+
+// Count 返回次数
+func Count(list []string) int { return count[key(list)] }
